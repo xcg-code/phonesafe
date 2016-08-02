17735dc51c525797da58221c73d4eceda8af9622
xcg-code <1450152983@qq.com>
2016-08-02 17:23:39 +0800

    the third commit brfore zidingyishuxing

---
+++ app/src/main/java/com/app/phonesafe/activities/SettingActivity.java
@@ -0,0 +1,7 @@
+package com.app.phonesafe.activities;
+
+/**
+ * Created by 14501_000 on 2016/8/1.
+ */
+public class SettingActivity {
+}
---
+++ app/src/main/java/com/app/phonesafe/view/FocusTextView.java
@@ -0,0 +1,7 @@
+package com.app.phonesafe.view;
+
+/**
+ * Created by 14501_000 on 2016/8/1.
+ */
+public class FocusTextView {
+}
