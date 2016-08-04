b9e313b2cf177fd7ff72433a7f8cc15fc95ada7a
xcg-code <1450152983@qq.com>
2016-08-04 18:05:34 +0800

    the 7 commit

--- app/src/main/java/com/app/phonesafe/activities/Setup3Activity.java
+++ app/src/main/java/com/app/phonesafe/activities/Setup3Activity.java
@@ -1,22 +1,46 @@
 package com.app.phonesafe.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import com.app.phonesafe.Config;
 import com.app.phonesafe.R;
+import com.app.phonesafe.utils.SPUtils;
 
 
 /**
  * Created by 14501_000 on 2016/8/3.
  */
 public class Setup3Activity extends Activity {
-    Button bt_next;
+    Button bt_next4;
+    Button bt_pre3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup3);
+        setContentView(R.layout.activity_setup4);
+        bt_pre3= (Button) findViewById(R.id.bt_pre3);
+        bt_next4= (Button) findViewById(R.id.bt_next4);
+        bt_pre3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Setup3Activity.this,Setup3Activity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        bt_next4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SPUtils.putBoolean(getApplicationContext(),Config.SETUP_OVER,true);
+                Intent intent=new Intent(Setup3Activity.this,SetupOverActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
 }
--- app/src/main/java/com/app/phonesafe/activities/Setup4Activity.java
+++ app/src/main/java/com/app/phonesafe/activities/Setup4Activity.java
@@ -1,24 +1,37 @@
 package com.app.phonesafe.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 
+import com.app.phonesafe.Config;
 import com.app.phonesafe.R;
+import com.app.phonesafe.utils.SPUtils;
 
 
 /**
  * Created by 14501_000 on 2016/8/3.
  */
 public class Setup4Activity extends Activity {
-    Button bt_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setup4);
-
-
+        setContentView(R.layout.activity_setup3);
 }
+    public void nextPage(View view){
+        Intent intent=new Intent(Setup4Activity.this,SetupOverActivity.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.next_in_anim, R.anim.next_out_anim);
+    }
 
+    public void prePage(View view){
+        SPUtils.putBoolean(getApplicationContext(),Config.SETUP_OVER,true);
+        Intent intent=new Intent(Setup4Activity.this,Setup2Activity.class);
+        startActivity(intent);
+        finish();
+        overridePendingTransition(R.anim.pre_in_anim,R.anim.pre_out_anim);
+    }
 }
